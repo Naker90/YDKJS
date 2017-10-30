a244322675ec6ad4c3c40a3b5149493b1963a5dc
naker90 <AntonioJesusSG96@gmail.com>
2017-10-30 21:15:59 +0000

    add new describe to Chapter 5 and green - simple example of clousure

--- src/Scope And Closures/Chapter 5: Scope Closure/chapter5.test.js
+++ src/Scope And Closures/Chapter 5: Scope Closure/chapter5.test.js
@@ -2,7 +2,7 @@ let expect = require('chai').expect;
 
 describe("Chapter 5: Scope Closure", () => {
 
-    it('simple example of clousure', function () {
+    it('simple example of closure', function () {
         function foo(){
             let a;
             function bar() {
